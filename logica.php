541774e971ec8c012a52c329a0b0b568853d3f0e
LauroNascimento <lauronascimento2013@gmail.com>
2017-05-05 09:28:10 -0300

    Upload dos arquivos da aula do Jeffinho
    aaa

---
+++ logica.php
@@ -0,0 +1,13 @@
+<?php
+
+$tratamento = ['Weeb', 'Wegee', 'Oráculo'];
+$qualidade = ['o Paladino', 'o colosso', 'o galinho de tênis'];
+$lugares = ['de Crippling Depression', 'de Araquari', 'do IFC'];
+$nomes = ['Jeffinho', 'Bruno', 'Adram', 'Lauro', 'Pedro'];
+
+$tratamento = $tratamentos[rand(0,2)];
+$qualidade = $qualidades[rand(0,2)];
+$lugar= $lugares[rand(0,2)];
+$nome = $nomes[rand(0,3)];
+
+?>
